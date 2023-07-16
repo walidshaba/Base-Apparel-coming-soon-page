5d25686b1f47457fa256437471b9ab6d5476d0ed
Musa Shaba <musamohshaba@gmail.com>
2023-07-16 18:20:54 +0100

    Getting there

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,7 +7,9 @@ const errorMessage = document.querySelector('form p.bastard');
 form.addEventListener('submit', (e) => {
   if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
     e.preventDefault();
-    errorIcon.classList.toggle('active');
-    errorMessage.classList.toggle('active');
+    // errorIcon.classList.toggle('active');
+    errorIcon.className = 'error active';
+    errorMessage.className = 'bastard active';
+    // errorMessage.classList.toggle('active');
   }
 });
